2c373380fa27473cb12e70625c799912152313fb
sirisart <mr.sirisart1999@gmail.com>
2025-12-15 09:53:42 +0700

    sent 1

---
+++ src/q01/grade.ts
@@ -0,0 +1,20 @@
+export{};
+let args = process.argv.slice(2);
+let n = Number(args[0]);
+if(n >= 80){
+    console.log(`Grade is A`);
+}else if (n >= 70){
+    console.log(`Grade is B`);
+}
+else if (n >= 60){
+    console.log(`Grade is C`);
+}
+else if (n >= 50){
+    console.log(`Grade is D`);
+}
+else if (n < 50){
+    console.log(`Grade is F`);
+}
+else {
+    console.log(`Invalid Input`);
+}
