73e8ec61f9d1f8f1b8d8d304a054d7a592faa294
sirisart <mr.sirisart1999@gmail.com>
2025-12-21 23:24:54 +0700

    sent13

---
+++ src/q09/factorial.ts
@@ -0,0 +1,12 @@
+export{};
+let args = process.argv.slice(2);
+let n = Number(args[0]);
+let fac = 1;
+if(n <= 0 || isNaN(n) || !Number.isInteger(n)){
+    console.log("Invalid Input");
+    process.exit();
+}
+for(let i = n;i > 0;i--){
+    fac *= i;
+}
+console.log(String(fac));
